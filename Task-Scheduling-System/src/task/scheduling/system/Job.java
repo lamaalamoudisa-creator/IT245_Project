bc060915d4593aeb13a9780a53a04ca117a2aabe
lamaalamoudisa-creator <lama.alamoudisa@gmail.com>
2026-07-20 15:15:52 +0300

    create class job

---
+++ Task-Scheduling-System/src/task/scheduling/system/Job.java
@@ -0,0 +1,25 @@
+package task.scheduling.system;
+
+public class Job {
+
+    private String name;
+    private int priority;
+
+    public Job(String name, int priority) {
+        this.name = name;
+        this.priority = priority;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    @Override
+    public String toString() {
+        return name + " (Priority: " + priority + ")";
+    }
+}
